d23a02517bc9d239f309a45a0b4bf6ac889c260b
Dickinson Bedoya Perez <dickinsonbp@gmail.com>
2024-09-25 13:18:29 +0200

    remove strict mode

--- Frontend/budgetinvoice-front/src/index.js
+++ Frontend/budgetinvoice-front/src/index.js
@@ -15,11 +15,9 @@ import "primeicons/primeicons.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
